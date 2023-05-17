4b19cbb26a3b6fe7472ac19ec18a67c3cd3b3fa8
shypang <smartdragon417@gmail.com>
2023-05-17 19:35:34 +0900

    Editing: Create menu

--- curl_test/create_menu.sh
+++ curl_test/create_menu.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 curl --location --request POST 'http://localhost:8080/admin/v01/menu/create' \
 --header 'Content-Type: application/json' \
---data-raw '{
+--data-raw '{   
+    "menu_id" : 1,
     "image_url":"https://dimg04.c-ctrip.com/images/0M71x120009orv5n7598E_Q60.jpg_.webp",
     "name":"MeatSashimi",
     "quantity" : 100,
@@ -8,3 +11,4 @@ curl --location --request POST 'http://localhost:8080/admin/v01/menu/create' \
     "recommendation":true ,
     "admin" : "yongari"
 }'
+
