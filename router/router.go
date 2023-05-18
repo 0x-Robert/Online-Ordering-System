6bb2a8fe550fd7fd39dad4890d40556a3eb653fd
shypang <smartdragon417@gmail.com>
2023-05-18 16:14:35 +0900

    edit router

--- router/router.go
+++ router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"fmt"
 	ctl "online-ordering-system/controller"
 
 	"github.com/gin-gonic/gin"
@@ -19,16 +20,33 @@ func NewRouter(ctl *ctl.Controller) (*Router, error) {
 // cross domain을 위해 사용
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		//~ 생략
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		//허용할 header 타입에 대해 열거
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, X-Forwarded-For, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		//허용할 method에 대해 열거
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
 		c.Next()
 	}
 }
 
 // 임의 인증을 위한 함수
-func LiteAuth() gin.HandlerFunc {
+func liteAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		//~ 생략
-		c.Next()
+		if c == nil {
+			c.Abort() // 미들웨어에서 사용, 이후 요청 중지
+			return
+		}
+		//http 헤더내 "Authorization" 폼의 데이터를 조회
+		auth := c.GetHeader("Authorization")
+		//실제 인증기능이 올수있다. 단순히 출력기능만 처리 현재는 출력예시
+		fmt.Println("Authorization-word ", auth)
+
+		c.Next() // 다음 요청 진행
 	}
 }
 
